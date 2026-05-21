8ca02633c711384e269391f40d6354b525b47fc8
Jorge <jorge.barrios.hurtado@gmail.com>
2026-05-21 09:44:28 +0200

    update py. file

--- src/data_collection.py
+++ src/data_collection.py
@@ -10,23 +10,23 @@ if not api_key:
     raise ValueError("TMDB_API_KEY not found in environment variables")
 
 # to test how it works and recall data from specific movies
-def get_movie_details(tmdb_id):
-    BASE_URL = "https://api.themoviedb.org/3"
-    url = f"{BASE_URL}/movie/{tmdb_id}"
-    params = {"api_key": API_KEY}
+def get_movie_details(tmdb_id, api_key = api_key):
+    base = "https://api.themoviedb.org/3"
+    url = f"{base}/movie/{tmdb_id}"
+    params = {"api_key": api_key}
     response = requests.get(url, params=params)
     return response.json()
 
-def get_movie_keywords(tmdb_id):
-    BASE_URL = "https://api.themoviedb.org/3"
-    url = f"{BASE_URL}/movie/{tmdb_id}/keywords"
-    params = {"api_key": API_KEY}
+def get_movie_keywords(tmdb_id, api_key = api_key):
+    base = "https://api.themoviedb.org/3"
+    url = f"{base}/movie/{tmdb_id}/keywords"
+    params = {"api_key": api_key}
     response = requests.get(url, params=params)
     return response.json()
 
 
 # to create the dataset
-def enrich_film(tmdb_id, api_key):
+def enrich_film(tmdb_id, api_key = api_key):
     base = "https://api.themoviedb.org/3/movie"
     
     details = requests.get(f"{base}/{tmdb_id}", 
